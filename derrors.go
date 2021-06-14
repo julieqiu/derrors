f601489ffd4147f9c22b2fd0e8611f175dbdeeb1
Julie Qiu <julie@golang.org>
2021-06-13 16:29:39 -1000

    derrors: update package

--- derrors.go
+++ derrors.go
@@ -1,24 +1,12 @@
-// Package derrors provides helper functions that are called on defer to wrap
-// an error.
+// Package derrors defines internal error values to categorize the different
+// types error semantics we support.
 package derrors
 
-import "fmt"
-
-// Add adds context to the error.
-// The result cannot be unwrapped to recover the original error.
-// It does nothing when *errp == nil.
-//
-// Example:
-//
-//	defer derrors.Add(&err, "copy(%s, %s)", src, dst)
-//
-// See Wrap for an equivalent function that allows
-// the result to be unwrapped.
-func Add(errp *error, format string, args ...interface{}) {
-	if *errp != nil {
-		*errp = fmt.Errorf("%s: %v", fmt.Sprintf(format, args...), *errp)
-	}
-}
+import (
+	"errors"
+	"fmt"
+	"runtime"
+)
 
 // Wrap adds context to the error and allows
 // unwrapping the result to recover the original error.
@@ -33,4 +21,40 @@ func Wrap(errp *error, format string, args ...interface{}) {
 	if *errp != nil {
 		*errp = fmt.Errorf("%s: %w", fmt.Sprintf(format, args...), *errp)
 	}
+}
+
+// WrapStack is like Wrap, but adds a stack trace if there isn't one already.
+func WrapStack(errp *error, format string, args ...interface{}) {
+	if *errp != nil {
+		if se := (*StackError)(nil); !errors.As(*errp, &se) {
+			*errp = NewStackError(*errp)
+		}
+		Wrap(errp, format, args...)
+	}
+}
+
+// StackError wraps an error and adds a stack trace.
+type StackError struct {
+	Stack []byte
+	err   error
+}
+
+// NewStackError returns a StackError, capturing a stack trace.
+func NewStackError(err error) *StackError {
+	// Limit the stack trace to 16K. Same value used in the errorreporting client,
+	// cloud.google.com/go@v0.66.0/errorreporting/errors.go.
+	var buf [16 * 1024]byte
+	n := runtime.Stack(buf[:], false)
+	return &StackError{
+		err:   err,
+		Stack: buf[:n],
+	}
+}
+
+func (e *StackError) Error() string {
+	return e.err.Error() // ignore the stack
+}
+
+func (e *StackError) Unwrap() error {
+	return e.err
 }
